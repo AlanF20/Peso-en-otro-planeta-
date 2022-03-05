0db71874a4af2501d45d633937fb96c9a7676700
AlanF20 <alanif@hotmail.com>
2022-03-04 23:32:48 -0600

    Añadiendo los documentos Html, Css y Js

---
+++ calcularPeso.js
@@ -0,0 +1,17 @@
+function CalcularPeso() {
+    let peso = parseInt(document.getElementById('peso').value);
+    const gravTierra = 9.8;
+    const gravMarte = 3.7;
+    const gravJupiter = 24.8;
+    if (document.getElementById('planets').value === 'Marte') {
+        let pesoMarte = (peso * gravMarte) / gravTierra;
+        pesoMarte = parseFloat(pesoMarte)
+        document.getElementById('answer').innerHTML = 'Su peso en Marte es de: <strong>' + pesoMarte.toFixed(2) + '</strong>'
+        
+    } else {
+        let pesoJupiter = (peso * gravJupiter) / gravTierra;
+        pesoJupiter = parseFloat(pesoJupiter)
+        document.getElementById('answer').innerHTML = 'Su peso en Jupiter es de: <strong>' + pesoJupiter.toFixed(2) + '</strong>'
+    }
+    
+}
